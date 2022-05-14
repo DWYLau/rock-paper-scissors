78f6f56647ef96bd0aed568d6bcb4e06a46d1731
Derek <dereklau15@gmail.com>
2022-05-14 21:51:09 +0900

    Finish loop and add alert messages for results

--- javascript.js
+++ javascript.js
@@ -1,15 +1,7 @@
-// Variable Declarations
-
-let options = '';
-let playerScore = 0;
-let computerScore = 0; 
-computerSelection = computerPlay(); // Parameter Assignment
-
-
 // Function - Computer Outputs
 
 function computerPlay() { 
-    num = Math.floor(Math.random() * 3);
+    let num = Math.floor(Math.random() * 3);
     if (num === 0) { 
         return 'rock';
     } else if (num === 1) {
@@ -20,30 +12,33 @@ function computerPlay() {
 
 // Rock, paper, scissors return values and increment of scores
 
-function playRound(playerSelection, computerSelection) {
-    if (playerSelection === 'rock' && (computerSelection === 0)) {
+let playerScore = 0; 
+let computerScore = 0; 
+
+function playRound(playerSelection, computerSelection) {    
+    if (playerSelection === 'rock' && (computerSelection === 'rock')) {
         return 'You draw! Two rocks!';
-    }   else if (playerSelection === 'rock' && (computerSelection === 1)) {
+    }   else if (playerSelection === 'rock' && (computerSelection === 'paper')) {
         computerScore++;
         return 'You lose! Rock loses to paper.';
-    }   else if (playerSelection === 'rock' && (computerSelection === 2)) {
+    }   else if (playerSelection === 'rock' && (computerSelection === 'scissor')) {
         playerScore++;
         return 'You win! Rock beats scissor.';
-    }   else if (playerSelection === 'paper' && (computerSelection === 0)) {
+    }   else if (playerSelection === 'paper' && (computerSelection === 'rock')) {
         playerScore++;
         return 'You win! Paper beats rock';
-    }   else if (playerSelection === 'paper' && (computerSelection === 1)) {
+    }   else if (playerSelection === 'paper' && (computerSelection === 'paper')) {
         return 'You draw! Two papers!';
-    }   else if (playerSelection === 'paper' && (computerSelection === 2)) {
+    }   else if (playerSelection === 'paper' && (computerSelection === 'scissor')) {
         computerScore++;
         return 'You lose! Paper loses to scissors.';
-    }   else if (playerSelection === 'scissor' && (computerSelection === 0)) {
+    }   else if (playerSelection === 'scissor' && (computerSelection === 'rock')) {
         computerScore++;
         return 'You lose! Scissor loses to rock.';
-    }   else if (playerSelection === 'scissor' && (computerSelection === 1)) {
+    }   else if (playerSelection === 'scissor' && (computerSelection === 'paper')) {
         playerScore++;
         return 'You win! Scissor beats paper';
-    }   else if (playerSelection === 'scissor' && (computerSelection === 2)) {
+    }   else if (playerSelection === 'scissor' && (computerSelection === 'scissor')) {
         return 'You draw! Two scissors!'
     }
 }  
@@ -51,11 +46,21 @@ function playRound(playerSelection, computerSelection) {
 // Game loops to 5 rounds
 
 function game() {
-    playRound(playerSelection, computerSelection); {
-        for (let score = 0; score < 5; i++);
-            console.log('Round ') + (i + 1);
-            console.log(playRound(playerSelection, computerSelection));
+    for (let i = 0; i < 5; i++) {
+        let playerSelection = prompt('Type: Rock, Paper, or Scissors!');
+        playerSelection = playerSelection.toLowerCase();
+        const computerSelection = computerPlay()
+        console.log(playRound(playerSelection, computerSelection))
+    }
+        if (playerScore > computerScore) {
+            alert (`You win! Final score: ${playerScore} to ${computerScore}`);
+        }   else if (computerScore > playerScore) {
+            alert (`You lose! Final score: ${playerScore} to ${computerScore}`);
+        }   else {
+            alert (`You tied! Final score: ${playerScore} to ${computerScore}`);
         }
+      
 }
 
-    
+
+game()
